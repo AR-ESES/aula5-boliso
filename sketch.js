aa4819782d17f071c000cd0d18efb9898a471552
boliso <102222437+boliso@users.noreply.github.com>
2022-04-27 18:29:42 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,17 +1,29 @@
 let laranjas;
 
 function setup() {
-	createCanvas(windowWidth, windowHeight);
-	background(0)
+    createCanvas(windowWidth, windowHeight);
+    background(0);
 
 }
 
 function draw() {
 
-	stroke(230);
-	strokeWeight(2);
-	noFill();
-	circle(width / 2, height / 2, height / 4);
+    background(0)
 
+    if(mouseY>=height/2 && mouseX<=width/2)
+    fill    (175,238,238); // azul
 
+    else if(mouseY<height/2 && mouseX<=width/2) 
+    fill    (255,0,0); // vermelho
+
+    else if(mouseY<height/2 && mouseX>width/2) 
+    fill    (50,205,50); // verde 
+
+    else{
+         noFill();
+    }
+
+    stroke(230);
+    strokeWeight(2);
+    circle(width / 2, height / 2, height / 4);
 }
